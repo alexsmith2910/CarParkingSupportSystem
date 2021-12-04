9012c68df0b54611b4746bd8b14ee393f38d029f
BuildTools <unconfigured@null.spigotmc.org>
2021-12-04 08:40:22 +0000

    Improved Ticket Generation

--- src/util/generate_ticket.py
+++ src/util/generate_ticket.py
@@ -5,18 +5,25 @@ class Ticket:
     _loaded = {}
     ticket_id = None
 
-    def __new__(cls, ticket):
-        if (cls_ticket := cls._loaded.get(ticket)) is not None:
+    def __new__(cls, ticket_id, _loaded_from_db=False):
+        if isinstance(ticket_id, str) and _loaded_from_db is False:
+            return None
+
+        if (cls_ticket := cls._loaded.get(ticket_id)) is not None:
             print(f"Returning existing client from cache ({cls_ticket})")
             return cls_ticket
 
         cls_ticket = super().__new__(cls)
-        cls._loaded[ticket] = cls_ticket
+        cls._loaded[ticket_id] = cls_ticket
         return cls_ticket
 
-    def __init__(self, ticket):
-        self.ticket_id = self.generate_ticket_id()
+    def __init__(self, ticket_id, _loaded_from_db=False):
+        self.ticket_id = str(ticket_id)
 
     @staticmethod
-    def generate_ticket_id():
-        return uuid4()
+    def generate_new_ticket():
+        return Ticket(uuid4())
+
+    @staticmethod
+    def load_from_database(ticket_id):
+        return Ticket(ticket_id, _loaded_from_db=True)
